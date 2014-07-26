51f18cbab87d50a82bf3943594e7aeafbfd6e2a9
John Sietsma <john.sietsma@gmail.com>
2014-07-27 09:38:21 +1000

    Add animator that responds to gaze.

---
+++ Assets/Cubehedron/Scripts/GazeAnimatorTriggers.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeAnimatorTriggers : MonoBehaviour
+{
+    [Tooltip( "The animator to set the triggers on. If not sute, use the Animator on this object." )]
+    [SerializeField] private Animator gazeAnimator;
+    [SerializeField] private string GazeEnterTrigger = Gaze.GazeEnterMessage;
+    [SerializeField] private string GazeExitTrigger = Gaze.GazeExitMessage;
+
+    void Awake()
+    {
+        if ( gazeAnimator == null ) { gazeAnimator = GetComponent<Animator>(); }
+        D.Assert( gazeAnimator, "Gaze animator hasn't been set or there is no Animator on this object." );
+    }
+
+    public void OnGazeEnter( GazeHit hit )
+    {
+        gazeAnimator.SetTrigger( GazeEnterTrigger );
+    }
+
+    public void OnGazeExit( GazeHit hit )
+    {
+        gazeAnimator.SetTrigger( GazeExitTrigger );
+    }
+}
