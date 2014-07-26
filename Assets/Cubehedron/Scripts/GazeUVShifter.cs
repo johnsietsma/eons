3ba2cbc1cb05fd35908a5ac87502305077dd9937
John Sietsma <john.sietsma@gmail.com>
2014-07-27 09:22:31 +1000

    Replaced PlayMaker on uv shifter. No more PlayMaker!

---
+++ Assets/Cubehedron/Scripts/GazeUVShifter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeUVShifter : MonoBehaviour
+{
+    [SerializeField] private Material offsetMaterial;
+    [SerializeField] private Vector2 uvOffsetPerSecond;
+
+    private Vector2 currentOffset;
+    private bool hasGaze = false;
+
+    void Update()
+    {
+        if( hasGaze ) {
+            currentOffset += uvOffsetPerSecond * Time.deltaTime;
+            offsetMaterial.mainTextureOffset = currentOffset;
+        }
+    }
+
+    public void OnGazeEnter( GazeHit hit )
+    {
+        hasGaze = true;
+    }
+
+    public void OnGazeExit( GazeHit hit )
+    {
+        hasGaze = false;
+    }
+}
