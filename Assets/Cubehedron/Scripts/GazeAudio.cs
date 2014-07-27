5a38898c8506365a5e8b28656c412752cec714ba
John Sietsma <john.sietsma@gmail.com>
2014-07-27 10:28:46 +1000

    Added gaze audio.

---
+++ Assets/Cubehedron/Scripts/GazeAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip gazeAudio;
+
+    void Awake()
+    {
+        D.Assert( audio!=null, "Gaze audio must have an audio source." );
+    }
+
+    public void OnGazeEnter( GazeHit hit )
+    {
+        audio.clip = gazeAudio;
+        audio.Play();
+    }
+
+    public void OnGazeExit( GazeHit hit )
+    {
+        audio.Stop();
+    }
+}
