f339a73eec588032f125ad169df6149495535559
John Sietsma <john.sietsma@gmail.com>
2014-08-02 20:37:37 +1000

    Added a mirror, Alexis' head follows the gaze.

---
+++ Assets/Cubehedron/Scripts/GazeRotationFollower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeRotationFollower : MonoBehaviour {
+
+    [SerializeField] private Gaze gazeToFollow;
+    [SerializeField] private Transform followingTransform;
+
+	void LateUpdate () {
+        followingTransform.rotation = gazeToFollow.GazeTransform.rotation;
+	}
+}
