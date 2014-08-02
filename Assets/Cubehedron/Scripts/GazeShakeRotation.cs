0aea57d66176968106a42a577827c113f7a4f377
John Sietsma <john.sietsma@gmail.com>
2014-08-02 19:52:41 +1000

    Added GazeShakeRotation and added it to scale prop.

---
+++ Assets/Cubehedron/Scripts/GazeShakeRotation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeShakeRotation : MonoBehaviour
+{
+    [SerializeField] private Vector3 amount;
+    [SerializeField] private float speed;
+    [SerializeField] private iTween.LoopType loopType;
+    [SerializeField] private GameObject shakeObject;
+    private bool hasGaze;
+    private Vector3 startScale;
+
+    void Awake()
+    {
+        if ( shakeObject == null ) { shakeObject = gameObject; }
+    }
+
+    public void OnGazeEnter( GazeHit hit )
+    {
+        iTween.ShakeRotation( shakeObject, iTween.Hash(
+                            "amount", amount,
+                            "speed", speed,
+                            "loopType", loopType
+                        )
+                      );
+    }
+
+    public void OnGazeExit( GazeHit hit )
+    {
+        iTween.Stop( "shake" );
+    }
+}
