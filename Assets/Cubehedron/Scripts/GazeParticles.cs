1fad3aa9e0b79cc38c76f28f15a4a06b86fd5d77
John Sietsma <john.sietsma@gmail.com>
2014-08-23 16:00:51 +1000

    Add Standard Assets particle effects. Add gaze enabled particles.

---
+++ Assets/Cubehedron/Scripts/GazeParticles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeParticles : MonoBehaviour
+{
+    [Tooltip("Use all particle systems in this object and it's children.")]
+    [SerializeField] private GameObject particlesParentObject;
+
+    private ParticleSystem[] particlesSystems;
+
+    void Awake()
+    {
+        D.Assert( particlesParentObject!=null, "Please set the parent object of the particle ssytem." );
+        particlesSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void OnGazeEnter( GazeHit hit )
+    {
+        foreach( var ps in particlesSystems ) {
+            ps.Play();
+        }
+    }
+
+    public void OnGazeExit( GazeHit hit )
+    {
+        foreach( var ps in particlesSystems ) {
+            ps.Stop();
+        }
+    }
+}
