e72090d2b6780728e493110a4979d02979c38023
John Sietsma <john.sietsma@gmail.com>
2014-08-24 12:14:36 +1000

    Added ambient light gaze.

---
+++ Assets/Cubehedron/Scripts/GazeAmbientLightTo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeAmbientLightTo : GazeBehaviour
+{
+    [SerializeField] private Color ambientColorTo;
+    [SerializeField] private float time = 1;
+
+    private Color startColor;
+
+    void Awake()
+    {
+        startColor = RenderSettings.ambientLight;
+    }
+
+    protected override void DoGazeEnter( GazeHit hit )
+    {
+        iTween.ValueTo( gameObject, iTween.Hash(
+                            "from", 0,
+                            "to", 1,
+                            "time", time,
+                            "onupdate", "LerpAmbientLight"
+                        )
+                      );
+    }
+
+    protected override void DoGazeExit( GazeHit hit )
+    {
+        iTween.ValueTo( gameObject, iTween.Hash(
+                            "from", 1,
+                            "to", 0,
+                            "time", time,
+                            "onupdate", "LerpAmbientLight"
+                        )
+                      );
+    }
+
+    private void LerpAmbientLight( float value )
+    {
+        RenderSettings.ambientLight = Color.Lerp( startColor, ambientColorTo, value );
+    }
+}
